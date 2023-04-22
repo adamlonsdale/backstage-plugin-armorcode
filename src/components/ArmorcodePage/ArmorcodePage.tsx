9bf274404ec0d38cd63cae09a8e37df280492bdd
Adam Lonsdale <adamlonsdale@me.com>
2023-04-22 23:43:02 +0100

    Temporarily remove styling

--- src/components/ArmorcodePage/ArmorcodePage.tsx
+++ src/components/ArmorcodePage/ArmorcodePage.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 import {
   Table,
   TableColumn,
   Progress,
-  EmptyState,
-  MissingAnnotationEmptyState,
+  EmptyState
 } from '@backstage/core-components';
 import Alert from '@material-ui/lab/Alert';
 import useAsync from 'react-use/lib/useAsync';
@@ -13,44 +12,40 @@ import LaunchSharp from '@material-ui/icons/LaunchSharp';
 import { useApi } from '@backstage/core-plugin-api';
 import { Link } from '@backstage/core-components';
 
-import { Chip } from '@material-ui/core';
+// import { Chip } from '@material-ui/core';
 
 import { armorcodeApiRef } from '../../api';
-import {
-  ARMORCODE_PRODUCT_ANNOTATION,
-  isArmorcodeAvailable
-} from '../../utils/commonUtil';
 
-const useStyles = makeStyles(theme => ({
-  chipLabel: {
-    color: theme.palette.common.white,
-  },
-  chipHigh: {
-    margin: 0,
-    backgroundColor: '#5a100c',
-  },
-  chipCritical: {
-    margin: 0,
-    backgroundColor: '#9c251f',
-  },
-  chipMedium: {
-    margin: 0,
-    backgroundColor: '#e78c87',
-  },
-  chipLow: {
-    backgroundColor: theme.palette.grey[500],
-  },
-  chipNone: {
-    backgroundColor: theme.palette.grey[300],
-  },
-}));
+// const useStyles = makeStyles(theme => ({
+//   chipLabel: {
+//     color: theme.palette.common.white,
+//   },
+//   chipHigh: {
+//     margin: 0,
+//     backgroundColor: '#5a100c',
+//   },
+//   chipCritical: {
+//     margin: 0,
+//     backgroundColor: '#9c251f',
+//   },
+//   chipMedium: {
+//     margin: 0,
+//     backgroundColor: '#e78c87',
+//   },
+//   chipLow: {
+//     backgroundColor: theme.palette.grey[500],
+//   },
+//   chipNone: {
+//     backgroundColor: theme.palette.grey[300],
+//   },
+// }));
 
 type DenseTableProps = {
   productList: any[];
 };
 
 export const DenseTable = ({ productList }: DenseTableProps) => {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const columns: TableColumn[] = [
     { title: 'ID', field: 'id' },
@@ -62,30 +57,30 @@ export const DenseTable = ({ productList }: DenseTableProps) => {
   ];
 
   const data = productList.map(item => {
-    let gateColor;
+    // let gateColor;
 
-    switch (item.vulnerabilityWithRemediation.severity) {
-      case 'CRITICAL': {
-        gateColor = classes.chipCritical;
-        break;
-      }
-      case 'HIGH': {
-        gateColor = classes.chipHigh;
-        break;
-      }
-      case 'MEDIUM': {
-        gateColor = classes.chipMedium;
-        break;
-      }
-      case 'LOW': {
-        gateColor = classes.chipLow;
-        break;
-      }
-      default: {
-        gateColor = classes.chipNone;
-        break;
-      }
-    }
+    // switch (item.vulnerabilityWithRemediation.severity) {
+    //   case 'CRITICAL': {
+    //     gateColor = classes.chipCritical;
+    //     break;
+    //   }
+    //   case 'HIGH': {
+    //     gateColor = classes.chipHigh;
+    //     break;
+    //   }
+    //   case 'MEDIUM': {
+    //     gateColor = classes.chipMedium;
+    //     break;
+    //   }
+    //   case 'LOW': {
+    //     gateColor = classes.chipLow;
+    //     break;
+    //   }
+    //   default: {
+    //     gateColor = classes.chipNone;
+    //     break;
+    //   }
+    // }
 
     return {
       id: item.id,
